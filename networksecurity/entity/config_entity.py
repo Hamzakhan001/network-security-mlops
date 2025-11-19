895446d86a74d508c17eac942850a0b911e8b399
Hamzakhan001 <spaceus9@gmail.com>
2025-11-19 17:46:18 +0000

    data ingestion pipeline done

--- networksecurity/entity/config_entity.py
+++ networksecurity/entity/config_entity.py
@@ -4,11 +4,13 @@ from networksecurity.constant import training_pipeline
 print(training_pipeline.PIPELINE_NAME)
 
 class TrainingPipelineConfig:
-    def __init__(self,timestamp=datetime.now()):
+    def __init__(self, timestamp: datetime | None = None):
+        if timestamp is None:
+            timestamp = datetime.now()
         timestamp = timestamp.strftime("%m_%d_%Y_%H_%M_%S")
         self.pipeline_name = training_pipeline.PIPELINE_NAME
         self.artifact_name = training_pipeline.ARTIFACT_DIR
-        self.artifact_dir = os.path.join(self.artifact_name,timestamp)
+        self.artifact_dir = os.path.join(self.artifact_name, timestamp)
         self.timestamp: str = timestamp
         
 
